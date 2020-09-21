05751b999c70c917ead728c3fb6ec6b2ff856ca2
Maanas Arora <maanasarora23@gmail.com>
2020-09-21 12:58:16 -0400

    Create and Demonstrate Photo Class

---
+++ photo.js
@@ -0,0 +1,34 @@
+class Photo {
+  width;
+  height;
+
+  constructor(width = 8, height = 10) {
+    this.width = width;
+    this.height = height;
+  }
+
+  price() {
+    if (width == 8 && height == 10) {
+      return 4;
+    } else if (width == 10 && height == 12) {
+      return 6;
+    } else {
+      return 10;
+    }
+  }
+
+  toString() {
+    return (
+      "This is a " +
+      this.width.toString() +
+      " by " +
+      this.height.toString() +
+      " and it costs " +
+      this.price().toString()
+    );
+  }
+}
+
+let newPhoto = Photo();
+
+console.log(newPhoto);
